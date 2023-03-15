cf3fa36822124222e0492d2e6b92632f8f959fe2
Doug Blanding <dblanding@gmail.com>
2023-03-15 15:19:26 -0400

    removed secrets

--- micropython_scripts/lights/secrets.py
+++ micropython_scripts/lights/secrets.py
@@ -1,6 +1,1 @@
-secrets = {'ssid': "NETGEAR90",
-           'wifi_password': "exoticgadfly005",
-           'tz_offset': -5,
-           'lat': 28.924626133816208,
-           'long': -81.96947174743894,
-           }
+secrets = {}
